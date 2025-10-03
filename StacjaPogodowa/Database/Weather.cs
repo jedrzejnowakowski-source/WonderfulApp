4a72bff0135f7b2c63820b317cd78100a5fa358f
nauczyciel <nauczyciel@ADAM>
2025-10-03 15:31:20 +0200

    init

---
+++ StacjaPogodowa/Database/Weather.cs
@@ -0,0 +1,9 @@
+﻿namespace Database
+{
+    public class Weather
+    {
+        public int Id { get; set; }
+        public string MeasuredBy { get; set; } = string.Empty;
+        public DateTime MeasurementDate { get; set; }
+    }
+}
