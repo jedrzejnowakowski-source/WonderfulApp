560ba08d739b4dc2bd01baf05e1e8fca3a0fdf67
valdyslav <przewodniczacy@ADAM>
2025-10-17 15:19:54 +0200

    Dodano klasę User i metodę ChangeWeather
    Wprowadzono następujące zmiany:
    - Dodano klasę `User` w przestrzeni nazw `Database` z właściwościami `Id` i `Name`.
    - Zaktualizowano `Program.cs`, dodając metodę `ChangeWeather`, obsługę obiektów `Weather` oraz komunikaty w konsoli.
    - Dodano referencję do projektu `Database` w pliku `StacjaPogodowa.csproj`.

--- StacjaPogodowa/StacjaPogodowa/Program.cs
+++ StacjaPogodowa/StacjaPogodowa/Program.cs
@@ -1,4 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Database;
 
+Console.WriteLine("Hello, World!");
 Console.ReadKey();
+
+var weather2 = new Weather();
+var weather5 = new Weather();
+
+weather5 = ChangeWeather(weather2);
+
+Console.WriteLine($"Weather2 measured by: {weather2.MeasuredBy}");
+Console.WriteLine($"Weather5 measured by: {weather5.MeasuredBy}");
+
+weather5 = weather2;
+
+Weather ChangeWeather(Weather weather)
+{
+    var objectToChange = new Weather();
+    Console.WriteLine($"Weather measured by: {weather.MeasuredBy}");
+    objectToChange.MeasuredBy = "Jan Kowalski";
+    Console.WriteLine($"Weather measured by: {weather.MeasuredBy}");
+
+    return objectToChange;
+}
