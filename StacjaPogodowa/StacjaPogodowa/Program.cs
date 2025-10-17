2be5ea311c71cb383e03977fe366f1ca95906971
valdyslav <przewodniczacy@ADAM>
2025-10-17 16:00:59 +0200

    Refaktoryzacja metody ChangeWeather
    Zmieniono implementację metody `ChangeWeather`, przenosząc ją do nowej klasy `Utils` w pliku `Utils.cs`.
    Dodano przestrzeń nazw `StacjaPogodowa` w pliku `Program.cs` oraz zaimportowano klasę `Utils`.
    Usunięto wcześniejszą implementację metody `ChangeWeather` z pliku `Program.cs`.

--- StacjaPogodowa/StacjaPogodowa/Program.cs
+++ StacjaPogodowa/StacjaPogodowa/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Database;
+using StacjaPogodowa;
 
 Console.WriteLine("Hello, World!");
 Console.ReadKey();
@@ -7,19 +8,10 @@ Console.ReadKey();
 var weather2 = new Weather();
 var weather5 = new Weather();
 
-weather5 = ChangeWeather(weather2);
+var utils = new Utils();
+weather5 = utils.ChangeWeather(weather2);
 
 Console.WriteLine($"Weather2 measured by: {weather2.MeasuredBy}");
 Console.WriteLine($"Weather5 measured by: {weather5.MeasuredBy}");
 
 weather5 = weather2;
-
-Weather ChangeWeather(Weather weather)
-{
-    var objectToChange = new Weather();
-    Console.WriteLine($"Weather measured by: {weather.MeasuredBy}");
-    objectToChange.MeasuredBy = "Jan Kowalski";
-    Console.WriteLine($"Weather measured by: {weather.MeasuredBy}");
-
-    return objectToChange;
-}
---
+++ StacjaPogodowa/StacjaPogodowa/Utils.cs
@@ -0,0 +1,17 @@
+﻿using Database;
+
+namespace StacjaPogodowa
+{
+    internal class Utils
+    {
+        internal Weather ChangeWeather(Weather weather)
+        {
+            var objectToChange = new Weather();
+            Console.WriteLine($"Weather measured by: {weather.MeasuredBy}");
+            objectToChange.MeasuredBy = "Jan Kowalski";
+            Console.WriteLine($"Weather measured by: {weather.MeasuredBy}");
+
+            return objectToChange;
+        }
+    }
+}
